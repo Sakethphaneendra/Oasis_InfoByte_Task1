79080892487d12bd154450c956f3a953c207a53e
unknown <sakethphaneendra@gmail.com>
2023-08-14 16:23:38 +0530

    Create the basic version for the oasis infobyte landing page

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.getElementById("mybtn").onclick = function(){
+
+    var myname = document.getElementById("search_box").value;
+   alert(myname)
+}
+function active(){
+    var myname = document.getElementById("search_box").value;
+    document.getElementById("dd").innerHTML= myname+ "<br> "+" ThankYou For Your Support Us 🎉🥳🥳🎉 ";
+
+  
+}
+
